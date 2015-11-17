aee5aacc418ad92ff1b0675e2198ac417366e902
tyler <tylerhines1992>
2015-11-17 13:32:33 -0500

    First round of refactor

--- js/javascript.js
+++ js/javascript.js
@@ -1,45 +1,44 @@
 var hasOperatorBeenClicked = false;
 
-var firstNumber = [];
-var operatorType = [];
-var secondNumber = [];
+var number = {first: "", second: "", operator: ""};
 
 function checkId(event) {
     var target = event.target;
 
     if ($(target).attr('class') == "number" && hasOperatorBeenClicked == true) {
-      secondNumber.push(target.value);
-      document.getElementById("screen").innerHTML = firstNumber + operatorType + secondNumber;
+      number.second += target.value;
+      document.getElementById("screen").innerHTML = number.first + number.operator + number.second;
     } else {
         if ($(target).attr('class') == "number") {
-          firstNumber.push(target.value);
-          // firstNumber.join("");
-          document.getElementById("screen").innerHTML = firstNumber;
+          // firstNumber.push(target.value);
+          number.first += target.value;
+          document.getElementById("screen").innerHTML = number.first;
         }
     }
     if ($(target).attr('class') == 'operator') {
       if (target.value == "=") {
-        operatorType.toString();
-        firstNumber = firstNumber.join("");
-        secondNumber = secondNumber.join("");
-        firstNumber = parseInt(firstNumber);
-        secondNumber = parseInt(secondNumber);
-        if (operatorType == "+") {
-          document.getElementById("screen").innerHTML = firstNumber + secondNumber;
-        } else if (operatorType == "-") {
-          document.getElementById("screen").innerHTML = firstNumber - secondNumber;
-        } else if (operatorType == "*") {
-          document.getElementById("screen").innerHTML = firstNumber * secondNumber;
-        } else if (operatorType == "/") {
-          document.getElementById("screen").innerHTML = firstNumber / secondNumber;
-        } else {
-          alert("does not compute");
+        switch (number.operator) {
+          case "+":
+            document.getElementById("screen").innerHTML = parseInt(number.first) + parseInt(number.second);
+            break;
+          case "-":
+            document.getElementById("screen").innerHTML = parseInt(number.first) - parseInt(number.second);
+            break;
+          case "*":
+            document.getElementById("screen").innerHTML = parseInt(number.first) * parseInt(number.second);
+            break;
+          case "/":
+            document.getElementById("screen").innerHTML = parseInt(number.first) / parseInt(number.second);
+            break;
+          default:
+            alert("Does not compute");
+            break;
         }
         hasOperatorBeenClicked = false;
       } else {
-          operatorType.push(target.value);
+          number.operator = target.value;
           hasOperatorBeenClicked = true;
-          document.getElementById("screen").innerHTML = firstNumber + operatorType;
+          document.getElementById("screen").innerHTML = number.first + number.operator;
         }
     }
 }
